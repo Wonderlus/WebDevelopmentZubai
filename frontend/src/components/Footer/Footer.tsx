79dcefcf66b7ea9f386bd066ec918c8f060f4278
Wonderlus <ilya.jiltzov@gmail.com>
2025-05-09 00:23:20 +0300

    added main page

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,21 @@
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
-    return <footer>Footer</footer>;
+    return (
+        <footer className={styles.container}>
+            <div className={styles.contact}>
+                <p>Наши контакты</p>
+                <div>
+                    <a href="https://telegram.org/">Telegram</a>
+                    <a href="https://whatsapp.com/">WhatsApp</a>
+                    <a href="">Phone Number</a>
+                </div>
+            </div>
+            <div className={styles.quality}>
+                <p>Делаем качественно</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- frontend/src/components/MainPage/MainPage.tsx
+++ frontend/src/components/MainPage/MainPage.tsx
@@ -1,7 +1,27 @@
 import styles from "./MainPage.module.scss";
 
 const MainPage = () => {
-    return <div className={styles.container}>Хуй</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>
+                Добро пожаловать на сайт Мебельного Диллера
+            </div>
+            <div className={styles.description}>
+                Здесь вы можете найти лучшие предложения для покупки мебели. Мы
+                также принимаем заказы!
+            </div>
+            <div className={styles.img}>
+                <p className={styles.img_title}>
+                    Ниже представлены некоторые экземпляры наших работ
+                </p>
+                <div className={styles.imgs}>
+                    <img src="/main/1.jpg" alt="Кровать" />
+                    <img src="/main/2.jpg" alt="Диван" />
+                    <img src="/main/3.jpg" alt="Дверь" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default MainPage;
